288b6234fda5fc5f08ea857ab352562b8af384cd
Yash Sharma <yash32860@gmail.com>
2025-02-22 15:13:20 +0530

    first commit to do ci-cd

---
+++ aws-CodePipeLine/src/main/java/com/aws/AwsCodePipeLineApplication.java
@@ -0,0 +1,13 @@
+package com.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipeLineApplication.class, args);
+	}
+
+}
